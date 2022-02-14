f7a7fb0b58618091eb60a520d35c57178f6b41ca
Dan Trickey <git@trickey.io>
2022-02-14 17:01:59 +0000

    Remove validation that is default

--- rtui/commands/metadata.py
+++ rtui/commands/metadata.py
@@ -15,20 +15,6 @@ class MetadataCommand(BaseCommand):
         for i, v in sorted(self._tui.astoria.metadata.__dict__.items()):
             self._tui.print(f"{i}: {v}")
 
-    @classmethod
-    def validate_args(cls, command: str, args: List[str]) -> List[str]:
-        """
-        Validate a list of potential arguments to the command.
-
-        :param command: The command that has been called
-        :param args: a list of arguments to validate.
-        :returns: A list of errors to display.
-        """
-        if len(args) == 0:
-            return []
-        else:
-            return [f"{command} does not expect any arguments."]
-
 
 class GetSetMetadataCommand(BaseCommand):
     """Abstract command that gets or sets a metadata attribute."""
--- rtui/commands/quit.py
+++ rtui/commands/quit.py
@@ -13,17 +13,3 @@ class QuitCommand(BaseCommand):
         """Execute the command."""
         self._tui.print("Goodbye!")
         self._tui.running = False
-
-    @classmethod
-    def validate_args(cls, command: str, args: List[str]) -> List[str]:
-        """
-        Validate a list of potential arguments to the command.
-
-        :param command: The command that has been called
-        :param args: a list of arguments to validate.
-        :returns: A list of errors to display.
-        """
-        if len(args) == 0:
-            return []
-        else:
-            return [f"Expected no arguments to {command}."]
